074dc279f7af26263ab5bd3d7d57a330f295b10f
ROON627 <runharun627@gmail.com>
2025-10-28 16:28:25 +0500

    chore(server): move maintenance scripts into server/scripts/

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -17,22 +17,3 @@ createRoot(document.getElementById('root')).render(
     </ToastProvider>
   </StrictMode>,
 )
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './styles/global.css'
-import App from './pages/App.jsx'
-import { ToastProvider } from './components/ToastContext'
-import { CartProvider } from './components/CartContext'
-import { SettingsProvider } from './components/SettingsContext'
-
-createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <ToastProvider>
-      <SettingsProvider>
-        <CartProvider>
-          <App />
-        </CartProvider>
-      </SettingsProvider>
-    </ToastProvider>
-  </StrictMode>,
-)
