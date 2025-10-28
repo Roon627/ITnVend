9b54ef2f0c89c60e4c7c990a1b29d2c2599189a5
ROON627 <runharun627@gmail.com>
2025-10-28 16:23:22 +0500

    Organize: move App into pages, add root main.jsx entry

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -1,7 +1,26 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './styles/global.css'
-import App from './App.jsx'
+import App from './pages/App.jsx'
+import { ToastProvider } from './components/ToastContext.jsx'
+import { CartProvider } from './components/CartContext.jsx'
+import { SettingsProvider } from './components/SettingsContext.jsx'
+
+createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <ToastProvider>
+      <SettingsProvider>
+        <CartProvider>
+          <App />
+        </CartProvider>
+      </SettingsProvider>
+    </ToastProvider>
+  </StrictMode>,
+)
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './styles/global.css'
+import App from './pages/App.jsx'
 import { ToastProvider } from './components/ToastContext'
 import { CartProvider } from './components/CartContext'
 import { SettingsProvider } from './components/SettingsContext'
